f96342c7eac9187970c422b30db4bb23285e823f
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2025-02-26 16:10:19 +0100

    bug fixed

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
-  BASE_URL: "https://api.openweathermap.org/data/2.5",
-  GEO: "http://api.openweathermap.org/geo/1.0",
   API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
+  GEO_API_URL: "https://api.openweathermap.org/geo/1.0",
+  WEATHER_API_URL: "https://api.openweathermap.org/data/2.5",
   DEFAULT_PARAMS: {
     units: "metric",
     appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
